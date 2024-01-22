be01843c30d3822b6e84a400ef47ec2f230aa406
Alex Shamrai <gibsonshamray@gmail.com>
2024-01-22 23:28:31 +0200

    Refactor pages structure

--- src/main/java/com/useinsider/pages/MainPage.java
+++ src/main/java/com/useinsider/pages/MainPage.java
@@ -2,27 +2,20 @@ package com.useinsider.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
+public class MainPage extends BasePage {
 
-
-public class MainPage {
-
-    private static final String MAIN_PAGE_URL = "https://useinsider.com/";
+    private static final String MAIN_PAGE_PATH = "/";
     private static final By ACCEPT_ALL_BUTTON = By.id("wt-cli-accept-all-btn");
 
     private final WebDriver driver;
-    private final WebDriverWait wait;
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofMillis(2000));
     }
 
     public MainPage open() {
-        driver.get(MAIN_PAGE_URL);
+        driver.get(BASE_URL + MAIN_PAGE_PATH);
         return new MainPage(driver);
     }
 
@@ -32,9 +25,8 @@ public class MainPage {
         return new MainPage(driver);
     }
 
-    // TODO move to BasePage
     public MainPage isLoaded() {
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("lang-dropdown")));
+        waitForLoading(driver);
         return new MainPage(driver);
     }
 }
